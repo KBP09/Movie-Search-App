5fbc22fbcd5f655a822e204d655cce378c428b99
KBP09 <kanakbpandey2023@gmail.com>
2024-07-05 15:33:06 +0530

    [Html/CSS and JS code]

---
+++ script.js
@@ -0,0 +1,13 @@
+const BASE_URL = "http://www.omdbapi.com/?t=john+wick&y=2014";
+const apiKey = "apikey=58b198a";
+
+const btn = document.querySelector("#browse-box");
+
+
+const getData = () =>{
+    const movieName = document.querySelector('')
+};
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    getData();
+});
